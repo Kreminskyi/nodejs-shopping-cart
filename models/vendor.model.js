e8ea3d8dcd48f04c007fbd155b9dcdc2a82e82ec
Kreminskyi <kreminsky@bigmir.net>
2018-02-18 13:08:46 +0200

    Revert "created all the schema as model files"
    This reverts commit 884ea60c6811c35a2c0a64d090cfa50990b154c2.

--- models/vendor.model.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-
-var VendorSchema = new Schema({
-    vendorName: { type: String, required: true },
-    address: { type: String, required: true }
-});
-
-module.exports = mongoose.model('Vendor', VendorSchema);
